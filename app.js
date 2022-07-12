1fd797aa9cb4676870ee12530846c308ac7c95b1
Sarthak <sarthakkul044777@gmail.com>
2022-07-12 16:16:26 +0530

    correct commit

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res)=> {
-    res.send('<h1>Express Demo App</h1> <h4>Message:Sucess</h4><p>v 1.0</p>')
+app.get('/', (req, res) => {
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
 })
 
-app.get('/products', (req,res)=> {
-    res.send([
-        {
-        productId: '101',
-        price: 100
-        },
-        {
-            productId: '102',
-            price: 200
-        }
-    ])
+app.get('/products', (req, res) => {
+  res.send([
+    {
+      productId: '101',
+      price: 100
+    },
+    {
+      productId: '102',
+      price: 150
+    }
+  ])
 })
 
 app.listen(port, ()=> {
-    console.log(`Demo ap is up and listening on port ${port}`);
+  console.log(`Demo app is up and listening to port: ${port}`);
 })
+ 
